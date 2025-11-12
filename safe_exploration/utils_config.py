0e50d45122c2e84f7147cfd4334b82de55814d88
jjagdt <jjagdt@student.ethz.ch>
2025-11-12 15:07:50 +0100

    Commenting out

--- safe_exploration/utils_config.py
+++ safe_exploration/utils_config.py
@@ -22,7 +22,7 @@ from .safempc_simple import SimpleSafeMPC
 from .ssm_cem.gal_concrete_dropout import GalConcreteDropoutSSM
 from .ssm_cem.gp_ssm_cem import GpCemSSM
 from .ssm_cem.ssm_cem import CemSSM, JunkDimensionsSSM
-from .ssm_cem.dropout_ssm_cem import McDropoutSSM
+# from .ssm_cem.dropout_ssm_cem import McDropoutSSM
 from .utils import dlqr, unavailable
 
 try:
